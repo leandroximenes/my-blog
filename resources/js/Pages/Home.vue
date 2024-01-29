89c5930d8545e70f364685813fcce5f5b44c1300
leandro <leandroj.r.ximenes@gmail.com>
2024-01-29 19:10:01 +0000

    prettier and eslint installed

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,5 +1,9 @@
-<script setup></script>
+<script setup>
+import { props } from '@inertiajs/inertia-vue3'
+
+const { user } = props.value
+</script>
 
 <template>
-    <h1 class="text-3xl font-bold underline">Home</h1>
+  <h1 class="text-3xl font-bold underline">Home</h1>
 </template>
