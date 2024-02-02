e48f653268d4685bfa3111f29b4a4fe984e07671
leandro <leandroj.r.ximenes@gmail.com>
2024-02-02 01:32:00 +0000

    let's try

--- resources/js/ssr.js
+++ resources/js/ssr.js
@@ -2,7 +2,7 @@ import { createInertiaApp } from '@inertiajs/vue3'
 import createServer from '@inertiajs/vue3/server'
 import { renderToString } from '@vue/server-renderer'
 import { createSSRApp, h } from 'vue'
-import 'virtual:windi.css'
+import './css/app.css'
 
 createServer((page) =>
   createInertiaApp({
