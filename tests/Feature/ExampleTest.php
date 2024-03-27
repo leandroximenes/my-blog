16dafe0d522b107a7b5d3403656e8882a7dfc85b
leandro <leandroj.r.ximenes@gmail.com>
2024-03-27 22:14:06 +0000

    tests corrected

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -12,8 +12,10 @@ class ExampleTest extends TestCase
      */
     public function test_the_application_returns_a_successful_response(): void
     {
-        $response = $this->get('/');
+        $this->artisan('migrate', ['--env' => 'testing']);
+        $this->artisan('db:seed', ['--env' => 'testing']);
 
+        $response = $this->get('/');
         $response->assertStatus(200);
     }
 }
